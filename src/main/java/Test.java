9bd56eae93957e0cc6bf99b5dd8f35ab5aa33091
Liko <13764384658@163.com>
2017-06-30 14:24:37 +0800

    数据库sql

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,17 +1,70 @@
 import com.demo.book.jdbc.model.dao.BookDao;
 import com.demo.book.jdbc.model.entity.Book;
+
+import com.demo.book.jdbc.model.entity.BookCatalog;
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import java.util.List;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+
 
 /**
  * Created by Administrator on 2017/6/27.
  */
 public class Test {
+
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext con= new ClassPathXmlApplicationContext("spring.xml");
-        BookDao dao = con.getBean(BookDao.class);
-        List<Book> books = dao.selectAll();
-        System.out.println(books);
+        try {
+            insertNewBook();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+    /*@Autowired
+    private BookDao bookDao;*/
+
+    /**
+     * 存储新书
+     */
+
+     static void insertNewBook() throws IOException {
+        ClassPathXmlApplicationContext con = new ClassPathXmlApplicationContext("spring.xml");
+        BookDao dao = con.getBean(BookDao.class);
+        Book book=new Book();
+        book.setBookName("魔界");
+        book.setAuthor("李奶奶");
+        int i = dao.insertBook(book);
+        //写入文本
+        //Connection connect = Jsoup.connect("http://www.17k.com/chapter/2477255/28942975.html");
+         Connection connect = Jsoup.connect("http://www.17k.com/chapter/2477255/28542679.html");
+        Document doc = connect.get();
+        Elements content = doc.select("div#chapterContentWapper");
+        String text = content.text();
+        String[] s = text.split("\\s+?");
+         String catalogName = s[0];
+         String filePath="D:\\Game\\Test";
+        String fileName="\\kk2.txt";
+        String file=filePath+fileName;
+        Writer filewriter = null;
+            filewriter = new FileWriter(file, true);
+            for (String ss : s) {
+
+                filewriter.write(ss);
+            }
+            //写入文本文件
+             filewriter.close();
+            System.out.println("成功");
+         BookCatalog bookCatalog=new BookCatalog();
+         bookCatalog.setFileDir(filePath);
+         bookCatalog.setBook(book);
+        bookCatalog.setCatalogName(catalogName);
+         dao.insertBookMenu(bookCatalog);
+
+     }
 }
--- src/main/java/com/demo/book/jdbc/model/dao/BookDao.java
+++ src/main/java/com/demo/book/jdbc/model/dao/BookDao.java
@@ -1,6 +1,8 @@
 package com.demo.book.jdbc.model.dao;
 
 import com.demo.book.jdbc.model.entity.Book;
+import com.demo.book.jdbc.model.entity.BookCatalog;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -8,6 +10,38 @@ import java.util.List;
  * Created by Administrator on 2017/6/27.
  */
 public interface BookDao {
+    /**
+     * 书名作者名模糊查询
+     * @param bookName
+     * @param author
+     * @return
+     */
+    Book selectBookByNameAndAuthor(@Param("bookName")String bookName, @Param("author") String author);
 
+    /**
+     * 查询全部书本信息
+     * @return
+     */
     List<Book> selectAll();
+
+    /**
+     * 添加新书
+     * @param book
+     * @return
+     */
+
+    int insertBook(Book book);
+
+    /**
+     * 存储书本章节目录信息
+     * @param bookCatalog
+     * @return
+     */
+    int insertBookMenu(BookCatalog bookCatalog);
+    /**
+     * 删除书本
+     * @param id
+     * @return
+     */
+    int deleteBook(Integer id);
 }
